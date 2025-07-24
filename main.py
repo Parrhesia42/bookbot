6cff4af33c499bc42d3e204e20149c3c48b653f2
parrhesia <arathron42@gmail.com>
2025-07-24 14:50:04 -0500

    stats done

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from stats import count_words
-
+from stats import char_count
 
 def main():
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
     num_words = count_words(book_text)
+    chars_dict = char_count(book_text)
     print(f"{num_words} words found in the document")
+    print(chars_dict)
 
 def get_book_text(filepath):
     with open(filepath) as f:
--- stats.py
+++ stats.py
@@ -1,3 +1,14 @@
 def count_words(book_text):
     return len(book_text.split())
-    
+
+
+def char_count(book_text):
+    count = {}
+    for char in book_text:
+        lowered = char.lower()
+        if lowered not in count:
+            count[lowered] = 1
+        else:
+            count[lowered] += 1
+    return count
+        
