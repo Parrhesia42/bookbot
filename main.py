5790002c121a323e5390047b8a4810bee817d88b
parrhesia <arathron42@gmail.com>
2025-07-24 16:26:40 -0500

    report formatting

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from stats import count_words
 from stats import char_count
+from stats import create_list
 
 def main():
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
     num_words = count_words(book_text)
     chars_dict = char_count(book_text)
-    print(f"{num_words} words found in the document")
-    print(chars_dict)
+    list_sort = create_list(chars_dict)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for dict in list_sort:
+        if dict["char"].isalpha():
+            print(f"{dict["char"]}: {dict["num"]}")
+    print("============= END ===============")
+    
 
 def get_book_text(filepath):
     with open(filepath) as f:
--- stats.py
+++ stats.py
@@ -11,4 +11,15 @@ def char_count(book_text):
         else:
             count[lowered] += 1
     return count
-        
+
+def create_list(chars_dict):
+    tmp_list = []
+    for key in chars_dict:
+        value = chars_dict[key]
+        tmp_list.append({"char": key, "num": value}) 
+    tmp_list.sort(reverse=True, key=sort_on)
+    return tmp_list
+
+
+def sort_on(items):
+    return items["num"]
